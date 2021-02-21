dc3e12b1ad4b28891713204c6178beb571660cd1
Nhat Nguyen <dsiver144@gmail.com>
2021-02-21 21:54:00 +0700

    added core

---
+++ DSI-Core.js
@@ -0,0 +1,199 @@
+//================================================================
+// * Plugin Name    : DSI-Core
+// - Last updated   : 19/2/2021
+//================================================================
+/*:
+ * @plugindesc v1.0 A helper plugin for DSI plugins.
+ * @author dsiver144
+*/
+
+// Parse SE
+var Imported = Imported || {};
+Imported.DSI_Core = 1.0;
+
+PluginManager.checkForNewVersion = function() {
+    var http = require('https');
+    var fs = require('fs');
+
+    var download = function(url, dest, cb) {
+        var file = fs.createWriteStream(dest);
+        http.get(url, function(response) {
+            response.pipe(file);
+            file.on('finish', function() {
+                file.close(cb);
+            });
+        });
+    }
+    download('https://raw.githubusercontent.com/dsiver144/mz-snipets/main/MZSnipets.js?token=ADXWAUGZ3PL5SHZO3F4YIWDAGJ2FG', '/js', (file)=>{
+        console.log(file);
+    })
+};
+
+PluginManager.checkForNewVersion();
+
+
+PluginManager.parseSE = function(param) {
+    param = JSON.parse(param);
+    param.volume = parseInt(param.volume);
+    param.pitch = parseInt(param.pitch);
+    param.pan = parseInt(param.pan);
+    return param;
+};
+
+PluginManager.processParameters = function(paramObject) {
+    paramObject = JsonEx.makeDeepCopy(paramObject);
+    for (k in paramObject) {
+        if (k.match(/(.+):(\w+)/i)) {
+            value = paramObject[k];
+            delete paramObject[k];
+            const paramName = RegExp.$1;
+            const paramType = RegExp.$2;
+            switch(paramType) {
+                case 'struct':
+                    value = JSON.parse(value);
+                    value = PluginManager.processParameters(value);
+                    break;
+                case 'num': case 'number':
+                    value = Number(value);
+                    break;
+                case 'arr': case 'note': case 'array':
+                    value = JSON.parse(value);
+                    break;
+                case 'vec': case 'vector':
+                    value = value.split(",").map(n => Number(n));
+                    break;
+                case 'str_vec':
+                    value = value.split(",");
+                    break;
+            }
+            paramObject[paramName] = value;
+            console.log(RegExp.$1, RegExp.$2);
+            
+        }
+    }
+    return paramObject;
+};
+
+// Return An Random Item From Array
+Array.prototype.randomizeItem = function() {
+    return this[Math.floor(Math.random() * this.length)];
+};
+ 
+var Easing = Easing || {}
+Easing.easeInOutExpo = function(t, b, c, d) {
+    if (t==0) return b;
+    if (t==d) return b+c;
+    if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+    return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+}
+Easing.easeOutExpo = function(t, b, c, d) {
+    return (t == d) ? b + c : c * (-Math.pow(2, -10 * t / d) + 1) + b;
+}
+Easing.easeInOutQuad = function(t, b, c, d) {
+    if ((t/=d/2) < 1) return c/2*t*t + b;
+    return -c/2 * ((--t)*(t-2) - 1) + b;
+}
+Easing.easeLinear = function(t, b, c, d) {
+    return c * t / d + b;
+}
+Easing.easeOutSine = function(t, b, c, d) {
+    return c * Math.sin(t / d * (Math.PI / 2)) + b;
+}
+Easing.easeInCirc = function(t, b, c, d) {
+    return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+}
+Easing.easeInCubic = function(t, b, c, d) {
+    return c * (t /= d) * t * t + b;
+}
+Easing.easeInOutCubic = function(t, b, c, d) {
+    if ((t /= d / 2) < 1) return c / 2 * t * t * t + b;
+    return c / 2 * ((t -= 2) * t * t + 2) + b;
+}
+Easing.easeInOutBack = function(t, b, c, d) {
+    s = 1.70158;
+    if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+    return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+}
+
+Easing.classes = [Sprite, Window];
+
+Easing.classes.forEach(className => {
+    className.prototype.startTween = function(settings, duration, onFinishCallBack, easingFunction, repeat) {
+        if (!easingFunction) {
+            easingFunction = Easing.easeLinear;
+        }
+        if (!repeat) repeat = false;
+        var tween = {}
+        tween.properties = {}
+        for (key in settings) {
+            tween.properties[key] = {beginValue: this[key], changeValue: (settings[key] - this[key])}
+        }
+        tween.frameCount = 0;
+        tween.duration = duration;
+        tween.onFinishCallBack = onFinishCallBack;
+        tween.easingFunction = easingFunction.bind(this);
+        tween.repeat = repeat;
+        tween.delayCount = 0;
+        // Chain functions
+        tween.delay = function(frames) {
+            this.delayCount = frames;
+            console.log(frames);
+            return this;
+        }
+        var self = this;
+        tween.ease = function(func) {
+            this.easingFunction = func.bind(self);
+            return this;
+        }
+        tween.onFinish = function(func) {
+            this.onFinishCallBack = func;
+            return this;
+        }
+        tween.repeatForever = function(bool) {
+            this.repeat = bool;
+            return this;
+        }
+        this._tweenObject = tween;
+        return tween
+    };
+    
+    className.prototype.onUpdate = function(callback) {
+        this._onUpdateCallback = callback.bind(this);
+    };
+    
+    className.prototype.removeTween = function() {
+        this._tweenObject = undefined;
+    };
+    
+    var DSI_SpriteTween_update = className.prototype.update;
+    className.prototype.update = function() {
+        DSI_SpriteTween_update.call(this);
+        if (this._onUpdateCallback) this._onUpdateCallback();
+        if (this._tweenObject) {
+            const tween = this._tweenObject;
+            if (tween.delayCount > 0) {
+                tween.delayCount -= 1;
+                return;
+            }
+            if (tween.frameCount < tween.duration) {
+                for (property in tween.properties) {
+                    var t = tween.frameCount;
+                    var b = tween.properties[property].beginValue;
+                    var c = tween.properties[property].changeValue;
+                    var d = tween.duration;
+                    this[property] = tween.easingFunction(t, b, c, d);
+                }
+                tween.frameCount += 1;
+            } else {
+                if (tween.onFinishCallBack) tween.onFinishCallBack.call(this);
+                if (tween.repeat) {
+                    tween.frameCount = 0;
+                } else {
+                    this._tweenObject = undefined;
+                }
+               
+            }
+        }
+    };
+})
+
--- MZSnipets.js
+++ MZSnipets.js
@@ -1,4 +1,29 @@
+/*~struct~SoundEffect:
+ * @param name
+ * @type file
+ * @dir audio/se/
+ * @desc Choose the name of SE you want to use.
+ *
+ * @param volume
+ * @default 70
+ * @desc Choose the volume value of the se
+ * 
+ * @param pitch
+ * @default 100
+ * @desc Choose the pitch value of the se
+ * 
+ * @param pan
+ * @default 0
+ * @desc Choose the pan value of the se
+ * 
+ */
 // Draw Sprite Texture To A Bitmap.
+PluginManager.parseSE = function(param) {
+    param = JSON.parse(param);
+    param.volume = parseInt(param.volume);
+    param.pitch = parseInt(param.pitch);
+    param.pan = parseInt(param.pan);
+};
 
 var sprite = new Sprite(new Bitmap(pw, ph));
 sourceBitmap.addLoadListener(()=>{
@@ -10,3 +35,26 @@ sourceBitmap.addLoadListener(()=>{
     const canvas = renderer.extract.canvas(renderTexture);
     targetBitmap.context.drawImage(canvas, dx, dy);
 })
+
+// Scene Template
+function Scene_Custom() {
+    this.initialize.apply(this, arguments);
+}
+
+Scene_Custom.prototype = Object.create(Scene_MenuBase.prototype);
+Scene_Custom.prototype.constructor = Scene_Custom;
+Scene_Custom.prototype.initialize = function() {
+    Scene_MenuBase.prototype.initialize.call(this);
+};
+
+Scene_Custom.prototype.create = function() {
+    Scene_MenuBase.prototype.create.call(this);
+};
+
+// MV Plugin Command
+
+var DSI_FG_Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    DSI_FG_Game_Interpreter_pluginCommand.apply(this, arguments);
+    
+}
